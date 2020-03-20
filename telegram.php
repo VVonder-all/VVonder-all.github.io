156d2b92482b041c6818e5708e6bc005e25b722e
VVonder-all <62436372+VVonder-all@users.noreply.github.com>
2020-03-20 22:08:10 +0200

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+/* https://api.telegram.org/bot1105355730:AAEY4As7XjWEOOYiJBH4ud_lplib0r_MDL4/getUpdates,
+где , ХХХХХХХХХХХХХХХХХХХХХХХХХХ - токен вашего бота,полученный ранее */
+
+$name = $_POST['user_name'];
+$message = $_POST['user_message'];
+$token = "1105355730:AAEY4As7XjWEOOYiJBH4ud_lplib0r_MDL4";
+$chat_id = "1149295077";
+$arr = array(
+	'Имя отправителя' => $name, 
+	'Сообщение' => $message
+);
+
+foreach ($arr as $key => $value) {
+	$txt .= "<br>" .$kay. "</br>" .$value. "%0A";
+};
+
+$sendToTeelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id} $parse_mode=html&text={$txt}","r");
+
+if ($sendToTeelegram) {
+	header('Location: thank-you.html');
+} else {
+	echo "Error";
+}
+?>
+
