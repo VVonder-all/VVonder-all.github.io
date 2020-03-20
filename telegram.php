75710c1927351075ea9c2b61d0b040423ed86d01
VVonder-all <62436372+VVonder-all@users.noreply.github.com>
2020-03-20 23:09:11 +0200

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,29 +1,37 @@
 <?php
 
-/* https://api.telegram.org/bot1105355730:AAEY4As7XjWEOOYiJBH4ud_lplib0r_MDL4/getUpdates,
-где , ХХХХХХХХХХХХХХХХХХХХХХХХХХ - токен вашего бота,полученный ранее */
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email']
-$token = "1105355730:AAEY4As7XjWEOOYiJBH4ud_lplib0r_MDL4";
-$chat_id = "-404497460";
+if (isset($_POST['form'])) { // Это ид кнопки на которую нажмут и сработает php скрипт - Убрать если просто сообщение при заходе на страницу ( внизу скобку убрать }
+
+$name = $_POST['user_name'];// это что ловим с формы 
+$phone = $_POST['user_phone']; // это что ловим с формы
+$email = $_POST['user_email']; // это что ловим с формы
+// Можно приписывать так если ловить не надо 
+// $zakaz = "Заказ с сайты XXX";
+
+$token = "1105355730:AAEY4As7XjWEOOYiJBH4ud_lplib0r_MDL4"; // Это ТОКЕН
+$chat_id = "-404497460"; // Это ИД группы
+
 $arr = array(
-	'Имя отправителя' => $name, 
-	'Телефон' => $phone
-	'Email' => $email
+
+// 'Заказ с сайты: ' => $zakaz,
+  'Телефон: ' => $phone,
+  'Имя: ' => $name,
+  'Мыло' => $email
 );
 
-foreach ($arr as $key => $value) {
-	$txt .= "<br>" .$kay. "</br>" .$value. "%0A";
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTeelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id} $parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTeelegram) {
-	header('Location: index.html');
+// Это условие Если отправлено редирект если нет  ошибка Можно убрать!
+if ($sendToTelegram) {
+  header('Location: url.html');
 } else {
-	echo "Error";
+  echo "Error";
+}
+// ид кнопки
 }
 ?>
-
